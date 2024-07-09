b34ca61193be7a29de1758e6744468053d43600d
salahatwa <salahsayedatwa@gmail.com>
2024-07-09 13:19:49 +0200

    Adding utility

--- src/main/java/com/api/common/utils/DateUtils.java
+++ src/main/java/com/api/common/utils/DateUtils.java
@@ -55,41 +55,70 @@ public class DateUtils {
 //            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 			Date dateStart = DATE_FORMAT.parse(start);
 			Date dateEnd = DATE_FORMAT.parse(end);
-			
+
 			Date currentDt = DATE_FORMAT.parse("2024-8-01");
 
 			LocalDate startDate = LocalDate.ofInstant(dateStart.toInstant(), ZoneId.systemDefault());
 			LocalDate endDate = LocalDate.ofInstant(dateEnd.toInstant(), ZoneId.systemDefault());
 			LocalDate current = LocalDate.ofInstant(currentDt.toInstant(), ZoneId.systemDefault());
-			return ((startDate.isEqual(current) || startDate.isAfter(current)||current.isAfter(startDate)))
-					&& ((endDate.isEqual(current) || endDate.isAfter(startDate))&&!current.isBefore(endDate));
+			return ((startDate.isEqual(current) || startDate.isAfter(current) || current.isAfter(startDate)))
+					&& ((endDate.isEqual(current) || endDate.isAfter(startDate)) && !current.isBefore(endDate));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		return false;
 	}
 
-	public static boolean isDateInBetween(String start, String end, final Date date){
-		
-		Date min=null;
-		Date max=null;
+	public static boolean isDateInBetween(String start, String end, final Date date) {
+
+		Date min = null;
+		Date max = null;
 		try {
 			min = DATE_FORMAT.parse(start);
-		  max = DATE_FORMAT.parse(end);
+			max = DATE_FORMAT.parse(end);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
+
+		return ((date.after(min) || date.equals(min)) && (date.before(max) || date.equals(max)));
+	}
+
+	public static boolean isFutureDate(String start,String end,  final Date currentDate) {
+
+		if(isExpiredDate(end, currentDate))
+			return !isExpiredDate(end, currentDate);
 		
-		
-	    return ((date.after(min) ||date.equals(min)) && (date.before(max)||date.equals(max)));
+		Date startDt = null;
+		try {
+			startDt = DATE_FORMAT.parse(start);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return startDt.after(currentDate);
 	}
 	
+	public static boolean isExpiredDate(String end,  final Date currentDate) {
+
+		Date endDt = null;
+		try {
+			endDt = DATE_FORMAT.parse(end);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return currentDate.after(endDt);
+	}
+
 	public static void main(String[] args) {
 //		System.out.println(validateDates("2024-7-7", "2024-7-31"));
-		
+
 		try {
 //			System.out.println(isDateInBetween("2024-7-8", "2024-7-31", DATE_FORMAT.parse("2024-8-1")));
-			System.out.println(isDateInBetween("2024-7-9", "2024-7-31", new Date()));
+//			System.out.println(isDateInBetween("2024-7-9", "2024-7-31", new Date()));
+			System.out.println(isExpiredDate("2024-8-25", DATE_FORMAT.parse("2024-8-25")));
+			
+			System.out.println(isFutureDate("2024-8-1","2024-8-27", DATE_FORMAT.parse("2024-7-1")));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
